8a56987ce72664b32d4f072230b2a25ccbccb1ec
RobertaMatera <materaroberta11@gmail.com>
2021-12-17 16:23:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import TvShows from "./components/TvShows";
 import MoviesRow from "./components/MoviesRow";
-import Footer from "./Components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
